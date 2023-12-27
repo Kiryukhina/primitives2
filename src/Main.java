00a131f2dcc6aa3d135c67e9b6984f885eeba28f
Kate <kiryuhinakatya@gmail.com>
2023-12-28 00:47:50 +0300

    Corrections

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,23 @@ public class Main {
 
     public static void main(String[] args) {
         int initialAccount = 73; //начальная сумма
-        int amount = 1200; //пополнение счета
+        int amount = 1250; //пополнение счета
 
         int bonus;
         if (amount > 1000) {
             bonus = 1;
-        }
-
-        if (amount < 1000) {
+        } else {
             bonus = 0;
         }
 
         int quantity = amount / 100; //количество бонусов
+        if (amount < 1000) {
+            bonus = initialAccount + 0;
+        }
         int result = quantity + initialAccount;
-
+        if (amount < 1000) {
+            result = initialAccount;
+        }
         System.out.println("Итоговая сумма: " + result);
 
 
