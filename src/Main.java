bec6c344c08734abe5fb524c14d577b3ac392bf0
Kate <kiryuhinakatya@gmail.com>
2023-12-28 06:16:41 +0300

    Add balance

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         int initialAccount = 73; //начальная сумма
-        int amount = 1250; //пополнение счета
+        int amount = 100; //пополнение счета
 
         int bonus;
         if (amount > 1000) {
@@ -15,9 +15,9 @@ public class Main {
         if (amount < 1000) {
             bonus = initialAccount + 0;
         }
-        int result = quantity + initialAccount;
+        int result = quantity + initialAccount + amount;
         if (amount < 1000) {
-            result = initialAccount;
+            result = initialAccount + amount;
         }
         System.out.println("Итоговая сумма: " + result);
 
